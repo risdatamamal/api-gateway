f8c7154f5bc1f269ebd7c319bda07e9d7eda1f28
Risda Tamam <risda123tamam@gmail.com>
2021-10-02 04:56:45 +0700

    Develop service user, and implementation JWT on API Gateway

--- routes/payments.js
+++ routes/payments.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
-const { APP_NAME } = process.env;
+var express = require('express')
+var router = express.Router()
+const { APP_NAME } = process.env
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('payments');
-});
+router.get('/', function (req, res, next) {
+  res.send('payments')
+})
 
-module.exports = router;
+module.exports = router
